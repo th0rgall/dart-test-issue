458ce654b2fbfaebcc0b1d97b3fb63721524338f
Thor Galle <thorgalle@gmail.com>
2022-05-25 13:21:39 +0300

    Add a problematic test case

--- test/dart_test_issue_test.dart
+++ test/dart_test_issue_test.dart
@@ -5,4 +5,18 @@ void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  late int a;
+  late int b;
+  late int result;
+
+  setUp(() {
+    a = 1;
+    b = 2;
+    result = a + b;
+  });
+
+  test('calculate late variable', () {
+    expect(result, 3);
+  });
 }
